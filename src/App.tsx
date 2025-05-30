42bc972fd67879fb6b5d5a795cc37898a6eac23f
ThallesLana <thalles.lana1@gmail.com>
2025-05-29 21:19:42 -0300

    :recycle: refactor: disabled verify for instagram in-browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,54 @@
-import { toast, ToastContainer } from 'react-toastify'
+import { ToastContainer } from 'react-toastify'
 import './App.css'
 import Copy from './components/copy/Copy'
 import CTA from './components/cta/CTA'
 import Footer from './components/footer/Footer'
 import GetLead from './components/getLead/GetLead'
 import ImpactTitle from './components/impactTitle/ImpactTitle'
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 
-function InstagramBlocker() {
-  const handleCopy = async () => {
-    try {
-      await navigator.clipboard.writeText(window.location.href)
-      toast.success('Link copiado para a área de transferência!');
-    } catch (err) {
-      alert('Não foi possível copiar o link.')
-      console.error('Erro ao copiar o link:', err)
-    }
-  }
+// function InstagramBlocker() {
+//   const handleCopy = async () => {
+//     try {
+//       await navigator.clipboard.writeText(window.location.href)
+//       toast.success('Link copiado para a área de transferência!');
+//     } catch (err) {
+//       alert('Não foi possível copiar o link.')
+//       console.error('Erro ao copiar o link:', err)
+//     }
+//   }
 
-  return (
-    <div style={{ padding: 20, textAlign: 'center' }}>
-      <h2>Abra no navegador para continuar</h2>
-      <p>
-        O Instagram bloqueia recursos de segurança nesta página.
-        <br />
-        Para continuar, clique nos <b>três pontinhos</b> no canto superior direito e escolha <b>“Abrir no navegador”</b>.<br /><br />
-        Ou, se preferir, copie o link clicando no botão abaixo e cole no seu navegador de preferência.
-      </p>
-      <br />
-      <button
-        onClick={handleCopy}
-        style={{ padding: '10px 20px', fontSize: 16, marginBottom: 20 }}
-      >
-        Copiar link
-      </button>
-    </div>
-  )
-}
+//   return (
+//     <div style={{ padding: 20, textAlign: 'center' }}>
+//       <h2>Abra no navegador para continuar</h2>
+//       <p>
+//         O Instagram bloqueia recursos de segurança nesta página.
+//         <br />
+//         Para continuar, clique nos <b>três pontinhos</b> no canto superior direito e escolha <b>“Abrir no navegador”</b>.<br /><br />
+//         Ou, se preferir, copie o link clicando no botão abaixo e cole no seu navegador de preferência.
+//       </p>
+//       <br />
+//       <button
+//         onClick={handleCopy}
+//         style={{ padding: '10px 20px', fontSize: 16, marginBottom: 20 }}
+//       >
+//         Copiar link
+//       </button>
+//     </div>
+//   )
+// }
 
 function App() {
-  const [isInstagram, setIsInstagram] = useState(false)
+  // const [isInstagram, setIsInstagram] = useState(false)
 
-  useEffect(() => {
-    const ua = navigator.userAgent || navigator.vendor || window.opera
-    setIsInstagram(ua.includes('Instagram'))
-  }, [])
+  // useEffect(() => {
+  //   const ua = navigator.userAgent || navigator.vendor || window.opera
+  //   setIsInstagram(ua.includes('Instagram'))
+  // }, [])
 
-  if (isInstagram) {
-    return <InstagramBlocker />
-  }
+  // if (isInstagram) {
+  //   return <InstagramBlocker />
+  // }
   return (
     <>
       <ImpactTitle />
