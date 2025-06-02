878f33f4bd192ecc105c836e279588f31d96f1d2
ThallesLana <thalles.lana1@gmail.com>
2025-06-02 17:07:20 -0300

    :recycle: refactor: remove comment code and include analytics from vercel.

--- src/App.tsx
+++ src/App.tsx
@@ -5,50 +5,9 @@ import CTA from './components/cta/CTA'
 import Footer from './components/footer/Footer'
 import GetLead from './components/getLead/GetLead'
 import ImpactTitle from './components/impactTitle/ImpactTitle'
-// import { useEffect, useState } from 'react'
-
-// function InstagramBlocker() {
-//   const handleCopy = async () => {
-//     try {
-//       await navigator.clipboard.writeText(window.location.href)
-//       toast.success('Link copiado para a área de transferência!');
-//     } catch (err) {
-//       alert('Não foi possível copiar o link.')
-//       console.error('Erro ao copiar o link:', err)
-//     }
-//   }
-
-//   return (
-//     <div style={{ padding: 20, textAlign: 'center' }}>
-//       <h2>Abra no navegador para continuar</h2>
-//       <p>
-//         O Instagram bloqueia recursos de segurança nesta página.
-//         <br />
-//         Para continuar, clique nos <b>três pontinhos</b> no canto superior direito e escolha <b>“Abrir no navegador”</b>.<br /><br />
-//         Ou, se preferir, copie o link clicando no botão abaixo e cole no seu navegador de preferência.
-//       </p>
-//       <br />
-//       <button
-//         onClick={handleCopy}
-//         style={{ padding: '10px 20px', fontSize: 16, marginBottom: 20 }}
-//       >
-//         Copiar link
-//       </button>
-//     </div>
-//   )
-// }
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
-  // const [isInstagram, setIsInstagram] = useState(false)
-
-  // useEffect(() => {
-  //   const ua = navigator.userAgent || navigator.vendor || window.opera
-  //   setIsInstagram(ua.includes('Instagram'))
-  // }, [])
-
-  // if (isInstagram) {
-  //   return <InstagramBlocker />
-  // }
   return (
     <>
       <ImpactTitle />
@@ -68,6 +27,7 @@ function App() {
         pauseOnHover
         theme="dark"
       />
+      <Analytics />
     </>
   )
 }
